8497be898c428a27ef60a90c45d4957abfdbc044
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2019-10-12 17:23:38 -0500

    Clase Contacto

---
+++ app/src/main/java/com/vegamex/crudbasico/Contacto.java
@@ -0,0 +1,62 @@
+package com.vegamex.crudbasico;
+
+import java.util.Calendar;
+
+public class Contacto {
+    int id;
+    String usuario;
+    String email;
+    String telefono;
+    Calendar fechaNacimiento;
+
+    public Contacto() {
+    }
+
+    public Contacto(int id, String usuario, String email, String telefono, Calendar fechaNacimiento) {
+        this.id = id;
+        this.usuario = usuario;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Calendar getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Calendar fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
