d968c6004d832c9b03a46ab15003ee81060e58a9
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2019-10-12 22:17:37 -0500

    ListView prueba

--- app/src/main/java/com/vegamex/crudbasico/MainActivity.java
+++ app/src/main/java/com/vegamex/crudbasico/MainActivity.java
@@ -13,11 +13,15 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +46,29 @@ public class MainActivity extends AppCompatActivity {
         long y = c.getTimeInMillis();
 
         DAOContacto daoContacto = new DAOContacto(this);
-        daoContacto.insert(new Contacto(0, "Oscar", "oscar@mail.com", "445", y));
-        daoContacto.insert(new Contacto(0, "Pablo", "pablo@mail.com", "445", y));
+        //daoContacto.insert(new Contacto(0, "Oscar", "oscar@mail.com", "445", y));
+        //daoContacto.insert(new Contacto(0, "Pablo", "pablo@mail.com", "445", y));
 
         for (Contacto contacto : daoContacto.getAll()){
             Toast.makeText(this,
                     contacto.usuario,
                     Toast.LENGTH_SHORT).show();
         }
+
+        lista = findViewById(R.id.lista);
+
+        SimpleCursorAdapter adp =
+                new SimpleCursorAdapter(
+                        this,
+                        android.R.layout.simple_list_item_2,
+                        daoContacto.getAllCursor(),
+                        new String[]{"_usuario","_email"},
+                        new int[]{android.R.id.text1, android.R.id.text2
+                        },
+                        SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+
+                );
+        lista.setAdapter(adp);
     }
 
     @Override
